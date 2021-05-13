c3d71c3bd01f0b02f54fce0f7f5108f2ef6008b7
Belen Cludius <belencludius@gmail.com>
2021-05-13 18:14:58 -0300

    Funcionalidad boton modo claro y fix script tag

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
+// PARA QUE QUEDE EN MODO CLARO
 
-const escribir = document.getElementById("idTxtSup");
+const btnDark = document.getElementById(`button-dark-appearance`);
+const appName = document.getElementById(`app-name`)
+const btnsContainer = document.getElementById('btns-container')
+const btnImg = document.getElementById('btn-img')
+const btnTxt = document.getElementById('btn-txt')
+const myMain =document.getElementById('my-main')
+const panelAsideImg = document.getElementById('panel-aside-img')
+const panelAsideTxt = document.getElementById('panel-aside-txt')
+const btnLight = document.getElementById('btn-light-appearance')
+
+btnDark.addEventListener('click', () => {
+  appName.style =`background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
+  btnsContainer.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
+  btnImg.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
+  btnTxt.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
+  btnDark.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
+  myMain.style = `background-color: rgb(230, 230, 230); color: rgb(50, 50, 50);`
+  panelAsideImg.style = 'background-color: rgb(240, 240, 240); color: rgb(50, 50, 50);'
+  panelAsideTxt.style = 'background-color: rgb(240, 240, 240); color: rgb(50, 50, 50);'
+  btnDark.style = 'display:none'
+  btnLight.style ='display:inline'
+
+})
+
+
+
+
+
+
+const escribirSup = document.getElementById("idTxtSup");
 const topText = document.getElementById("topText")
-const cambiarTexto = ()=>{
-    topText.innerText=escribir.value;
-};
+const escribirInf = document.getElementById('idTxtInf')
+const bottomText = document.getElementById('bottomText')
 
-escribir.addEventListener("input",cambiarTexto)
+const cambiarTexto = ()=>{
+    topText.innerHTML = escribirSup.value;
+    bottomText.innerHTML = escribirInf.value;
+ };
+ escribirSup.addEventListener("input",cambiarTexto)
+ escribirInf.addEventListener("input",cambiartexto)
+
+ 
+
+
+
+
+
