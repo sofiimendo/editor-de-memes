7fd24c9ee8a58aa0ce8309c8c26664d1f3ac3482
Tete <tete.paez.007@gmail.com>
2021-05-15 13:01:33 -0300

    modificacion  de estilos

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-
-const escribir = document.getElementById("idTxtSup");
+/*
+-------------TETX ASIDE--------------
+-------------top text-----------------
+const getTopText = document.getElementById("topText");
+console.log (getTopText);
+/*
+const cambiarTexto = ()=>{
+    topText.innerText=getTopText.value;
+};
+getTopText.addEventListener("click,cambiarTexto);*/
+/*
+const escribir = document.getElementById("TxtSup");
 const topText = document.getElementById("topText")
 const cambiarTexto = ()=>{
     topText.innerText=escribir.value;
 };
-
-escribir.addEventListener("input",cambiarTexto)
+escribir.addEventListener("input",cambiarTexto);
+-------------bottom text-----------------/*const getTopText = document.getElementById("topText");
+console.log (getTopText);
+/*
+const cambiarTexto = ()=>{
+    topText.innerText=getTopText.value;
+};
+getTopText.addEventListener("click,cambiarTexto);*/
+/*
+const escribir = document.getElementById("TxtSup");
+const topText = document.getElementById("topText")
+const cambiarTexto = ()=>{
+    topText.innerText=escribir.value;
+};
+escribir.addEventListener("input",cambiarTexto);*/
+//-------------IMAGE ASIDE--------------
+//-------------IMAGE ASIDE--------------
+//-------------IMAGE ASIDE--------------
+const getTheUrl = document.getElementById(idUrl);
+const pasteTheUrl = document.getElementById(imageMeme);
+const placeTheImage = ()=>{
+    pasteTheUrl.innerText=getTheUrl.value;
+};
