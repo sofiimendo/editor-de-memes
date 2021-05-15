9b6b1c69643f82d2f9e8e2e8033316dabd12d580
sofimmendoza@gmail.com <sofimmendoza@gmail.com>
2021-05-15 13:45:49 -0300

    filtros img y background img

--- script.js
+++ script.js
@@ -1,49 +1,77 @@
 // PARA QUE QUEDE EN MODO CLARO
-
-const btnDark = document.getElementById(`button-dark-appearance`);
-const appName = document.getElementById(`app-name`)
-const btnsContainer = document.getElementById('btns-container')
-const btnImg = document.getElementById('btn-img')
-const btnTxt = document.getElementById('btn-txt')
-const myMain =document.getElementById('my-main')
-const panelAsideImg = document.getElementById('panel-aside-img')
-const panelAsideTxt = document.getElementById('panel-aside-txt')
-const btnLight = document.getElementById('btn-light-appearance')
+const imgMeme = document.getElementById('imgMeme');
+const btnDark = document.getElementById('button-dark-appearance');
+const textTheme = document.getElementById('text-theme');
+const appName = document.getElementById('app-name');
+const btnsContainer = document.getElementById('btns-container');
+const btnImg = document.getElementById('btn-img');
+const btnTxt = document.getElementById('btn-txt');
+const myMain = document.getElementById('my-main');
+const panelAsideImg = document.getElementById('panel-aside-img');
+const panelAsideTxt = document.getElementById('panel-aside-txt');
+const body = document.querySelector('body');
 
 btnDark.addEventListener('click', () => {
-  appName.style =`background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
-  btnsContainer.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
-  btnImg.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
-  btnTxt.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
-  btnDark.style = `background-color: rgb(220, 220, 220); color: rgb(50, 50, 50);`
-  myMain.style = `background-color: rgb(230, 230, 230); color: rgb(50, 50, 50);`
-  panelAsideImg.style = 'background-color: rgb(240, 240, 240); color: rgb(50, 50, 50);'
-  panelAsideTxt.style = 'background-color: rgb(240, 240, 240); color: rgb(50, 50, 50);'
-  btnDark.style = 'display:none'
-  btnLight.style ='display:inline'
-
+  body.classList.toggle("dark");
+  body.classList.toggle("light");
+  if(body.classList.contains("dark")){
+    textTheme.innerText = "Modo Claro"
+  }else{
+    textTheme.innerText = "Modo Oscuro"
+  }
 })
 
-
-
-
-
-
+// cambiar texto (probemos desp de hacerlas x separado)
 const escribirSup = document.getElementById("idTxtSup");
 const topText = document.getElementById("topText")
 const escribirInf = document.getElementById('idTxtInf')
 const bottomText = document.getElementById('bottomText')
 
-const cambiarTexto = ()=>{
-    topText.innerHTML = escribirSup.value;
-    bottomText.innerHTML = escribirInf.value;
- };
- escribirSup.addEventListener("input",cambiarTexto)
- escribirInf.addEventListener("input",cambiartexto)
+const cambiarTexto = () => {
+  topText.innerHTML = escribirSup.value;
+  bottomText.innerHTML = escribirInf.value;
+};
+escribirSup.addEventListener("input", cambiarTexto)
+escribirInf.addEventListener("input", cambiarTexto)
 
- 
+//Traer imagen
+const urlImg = document.getElementById('imgUrl');
+urlImg.addEventListener('change',() => {
+  imgMeme.style.backgroundImage = `url('${urlImg.value}')`;
+})
 
+imgMeme.style.backgroundImage = `url('https://i.pinimg.com/originals/08/99/ea/0899eafb344f09b3656c18938718e1ff.jpg')`;
 
+//Background filter:
+const backgroundFilter = document.getElementById('imgBackgroundFilter');
+backgroundFilter.addEventListener('change', () => {
+  imgMeme.style.backgroundBlendMode = backgroundFilter.value
+})
 
+//Inputs filtros Imagenes:
+const brillo = document.getElementById('brightness');
+const opacidad = document.getElementById('opacity');
+const contraste = document.getElementById('contrast');
+const desenfoque = document.getElementById('blur');
+const escalaGrises = document.getElementById('grayScale');
+const sepia = document.getElementById('sepia');
+const hue = document.getElementById('hueRotation');
+const saturado = document.getElementById('saturate');
+const negativo = document.getElementById('invert');
+const filtros = document.getElementsByClassName('filter');
 
-
+const valores = () => {
+  let brilloValue = brillo.value;
+  let opacidadValue = opacidad.value;
+  let contrasteValue = contraste.value;
+  let desenfoqueValue = desenfoque.value;
+  let escalaGrisesValue = escalaGrises.value;
+  let sepiaValue = sepia.value;
+  let hueValue = hue.value;
+  let saturadoValue = saturado.value;
+  let negativoValue = negativo.value;
+  imgMeme.style.filter = `brightness(${brilloValue}) opacity(${opacidadValue}) contrast(${contrasteValue}%) blur(${desenfoqueValue}px) grayscale(${escalaGrisesValue}%) sepia(${sepiaValue}%) hue-rotate(${hueValue}deg) saturate(${saturadoValue}%) invert(${negativoValue})`;
+}
+for (let i = 0; i < filtros.length; i++) {
+  filtros[i].addEventListener('input',valores);
+}
